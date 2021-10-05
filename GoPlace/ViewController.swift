df090657d0c6329d216d796c3baef556a22e9417
johnny <johnnyjesuscj@gmail.com>
2021-10-04 23:47:51 -0400

    Initial Commit

---
+++ GoPlace/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoPlace
+//
+//  Created by user194452 on 10/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
